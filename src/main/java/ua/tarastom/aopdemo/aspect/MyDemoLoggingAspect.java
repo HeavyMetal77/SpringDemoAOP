19f449ed4c910ecb6c3410b72033773cd4af857e
HeavyMetal77 <ttm2006@ukr.net>
2019-12-31 18:40:33 +0200

    AOP Read Method Arguments with JoinPoints

--- src/main/java/ua/tarastom/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/ua/tarastom/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,7 +1,10 @@
 package ua.tarastom.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -56,5 +59,19 @@ public class MyDemoLoggingAspect {
     public void beforeAddAccountAdviceAnyClassAnyMethod() {
         System.out.println("=====>>>>> Executing @Before advice AddAccountAdviceAnyClassAnyMethod....");
         System.out.println("========== order 2");
+    }
+
+    @Before("execution(* ua.tarastom.aopdemo.dao.*.*(..))") //*.* - любой клас и любой метод
+    public void beforeAddAccountJoinPoint(JoinPoint joinPoint) {
+        System.out.println("=====>>>>> Executing @Before advice beforeAddAccountJoinPoint....");
+        System.out.println("========== order 2");
+
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature(); //получаем сигнатуру метода
+        System.out.println("Signature of method: " + signature);
+
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            System.out.println("Parameters of method: " + arg);
+        }
     }
 }
